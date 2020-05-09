c154f671e246ec773b854c0b141ef42c930aa4c6
Armin <arminlanger@gmail.com>
2020-05-09 14:52:14 +0200

    MakeYourFirst2DGame_GDQuest/ als beispiel hochgeladen

---
+++ MakeYourFirst2DGame_GDQuest/src/Objects/Coin.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+
+
+
+
+func _on_Coin_body_entered(body: PhysicsBody2D) -> void:
+	anim_player.play("fade_out")
+	
