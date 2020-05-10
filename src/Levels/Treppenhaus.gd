f070a03d19ae9b5aaac5b37ae0b38052bb67d603
Armin <arminlanger@gmail.com>
2020-05-10 14:23:53 +0200

    Die Oma ist jetzt funktionierend und läuft das Treppenhaus auf und ab indem sie an jeder Tür ihren Layer toggelt.

---
+++ src/Levels/Treppenhaus.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+#	$Wohnungstuer.connect("DoorCollision", $Oma, "_on_Door_Collision")
+#	$Wohnungstuer2.connect("DoorCollision", $Oma, "_on_Door_Collision")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
