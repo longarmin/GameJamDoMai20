d8830a6b831b4b3a72fc2ff3176810f7d799e61a
Sebastian Thon <sthon@hotmail.de>
2022-06-05 12:11:31 +0200

    Events sind nun Quests und einige Optimierungen

--- src/Bewohner/AIEvent.gd
+++
@@ -1,9 +0,0 @@
-class_name AIEvent
-
-var target: Node2D
-var iMax_Event_time: int
-
-
-func _init(eventTarget: Node2D, iMaxEventTime: int):
-	target = eventTarget
-	iMax_Event_time = iMaxEventTime
--- src/Bewohner/Neighbour.gd
+++ src/Bewohner/Neighbour.gd
@@ -10,7 +10,7 @@ const STANDARD_TIME_IDLE := 15
 var bAllowDropOnHalde: bool = true
 
 onready var nameLabel: Label = $Label
-onready var eventManager: EventManager = $EventManager
+onready var questManager: QuestManager = $QuestManager
 
 
 func _ready() -> void:
@@ -27,15 +27,15 @@ func instanciate(homeNew: Wohnung, sNeighbourName: String, fSpeedNew: float = fS
 
 func _on_Hitbox_area_entered(area: Area2D) -> void:
 	._on_Hitbox_area_entered(area)
-	if eventManager.current_event != null:
-		if area is Dump && abs(eventManager.current_event.target.position.x - area.position.x) < 5:
+	if questManager.current_quest != null:
+		if area is Dump && abs(questManager.current_quest.target.position.x - area.position.x) < 5:
 			var message = Message.new(2, "Neighbour on Dump", self)
 			stateMachine.respond_to(message)
 
 		# Check, ob Neighbour wieder in seine Flat zurueck soll:
-		if area == eventManager.current_event.target:
+		if area == questManager.current_quest.target:
 			# neues Ziel setzen: Flat des Neighbour
-			eventManager.remove_current_event()
+			questManager.remove_current_quest()
 
 	else: #if target equals 0
 		if area == home:
@@ -52,8 +52,8 @@ func _on_Hitbox_area_exited(area: Area2D) -> void:
 
 
 func activate_new_target() -> bool:
-	eventManager.activate_new_event()
-	if eventManager.current_event:
+	questManager.activate_new_quest()
+	if questManager.current_quest:
 		return true
 	else:
 		return false
